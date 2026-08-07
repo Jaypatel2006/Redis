3e6f4840ec8aa2b8cf584a22fc19b9333c18ef96
jaypatel2006 <jayspatel190506@gmail.com>
2026-08-08 00:16:14 +0530

    simple binary protocol read write function implemented

--- tcp_server.cpp
+++ tcp_server.cpp
@@ -2,19 +2,50 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <assert.h>
 
-static void do_something(int connfd){
+static void do_something(int connfd)
+{
     char rbuf[64] = {};
-    ssize_t n = read(connfd,rbuf,sizeof(rbuf) - 1);
-    if(n<0){
-        std::cout<<"Error while reading ... "<<"\n";
+    ssize_t n = read(connfd, rbuf, sizeof(rbuf) - 1);
+    if (n < 0)
+    {
+        std::cout << "Error while reading ... " << "\n";
         return;
     }
-    std::cout<<"Client Says  : "<<rbuf<<"\n";
+    std::cout << "Client Says  : " << rbuf << "\n";
     char wbuf[] = "Welcome";
-    write(connfd,wbuf,sizeof(wbuf));
+    write(connfd, wbuf, sizeof(wbuf));
+}
+
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        int rv = read(fd, buf, n);
+        if (rv <= 0)
+        {
+            return -1;
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
 
+static int32_t write_all(int fd, const char *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
 }
 
 int main()
@@ -29,35 +60,46 @@ int main()
     addr.sin_port = htons(1234);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-   
-    int rv = bind(fd , (const struct sockaddr*)&addr,sizeof(addr));
-    if(rv){
-        std::cout<<"Error While Binding"<<"\n";
-    }else{
-        std::cout<<"Bind Successfully.."<<"\n";
+    int rv = bind(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv)
+    {
+        std::cout << "Error While Binding" << "\n";
+    }
+    else
+    {
+        std::cout << "Bind Successfully.." << "\n";
     }
 
-    rv = listen(fd,SOMAXCONN);
-    if(rv){
-        std::cout<<"Error while listening.."<<"\n";
-    }else{
-        std::cout<<"Listening on port : "<<ntohs(addr.sin_port)<<"\n";
+    rv = listen(fd, SOMAXCONN);
+    if (rv)
+    {
+        std::cout << "Error while listening.." << "\n";
+    }
+    else
+    {
+        std::cout << "Listening on port : " << ntohs(addr.sin_port) << "\n";
     }
 
-    while(true){
+    while (true)
+    {
         struct sockaddr_in client_addr = {};
         socklen_t addrlen = sizeof(client_addr);
-        int connfd = accept(fd,(struct sockaddr*) &client_addr,&addrlen);
-        if(connfd < 0){
+        int connfd = accept(fd, (struct sockaddr *)&client_addr, &addrlen);
+        if (connfd < 0)
+        {
             continue;
         }
-        do_something(connfd);
 
+        while (true)
+        {
+            int32_t err = one_req(connfd);
+            if (err)
+            {
+                break;
+            }
+        }
+        close(connfd);
     }
-
-
-
-    
 
     return 0;
 }
