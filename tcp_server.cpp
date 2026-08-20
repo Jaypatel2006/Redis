582fa1dcb03bfe67750845bb008785d7e5da6cdc
jaypatel2006 <jayspatel190506@gmail.com>
2026-08-20 23:59:01 +0530

    Simple Protocol for get set and delete commands implemented

--- tcp_server.cpp
+++ tcp_server.cpp
@@ -15,6 +15,7 @@
 // C++
 #include <vector>
 #include<iostream>
+#include <map>
 
 
 const size_t k_max_msg = 4096;
@@ -29,6 +30,20 @@ struct Conn{
 
 };
 
+struct Response{
+    uint32_t status = 0;
+    std::vector<uint8_t> data;
+
+};
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,    // error
+    RES_NX = 2,     // key not found
+};
+
+
+static std::map<std::string, std::string> g_data;
 
 static void
 buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) {
@@ -71,25 +86,111 @@ static Conn *handle_accept(int fd) {
     return conn;
 }
 
-static bool try_one_req(Conn* conn){
-    if(conn->incoming.size() < 4){
+bool read_32(uint8_t *&curr,const uint8_t* end,uint32_t& out){
+    if(curr + 4 > end){
         return false;
+    }
+    memcpy(&out,curr,4);
+    curr += 4;
+    return true;
+}
+
+
+bool read_str(uint8_t *&curr,const uint8_t* end,uint32_t n,std::string &out){
+    if(curr+n > end){
+        return false;
+    }
+    out.assign(curr,curr+n);
+    curr+=n;
+    return true;
+
+} 
+
+
+
+static void do_request(std::vector<std::string> &cmd, Response &out) {
+    if (cmd.size() == 2 && cmd[0] == "get") {
+        auto it = g_data.find(cmd[1]);
+        if (it == g_data.end()) {
+            out.status = RES_NX;    // not found
+            return;
+        }
+        const std::string &val = it->second;
+        out.data.assign(val.begin(), val.end());
+    } else if (cmd.size() == 3 && cmd[0] == "set") {
+        g_data[cmd[1]].swap(cmd[2]);
+    } else if (cmd.size() == 2 && cmd[0] == "del") {
+        g_data.erase(cmd[1]);
+    } else {
+        out.status = RES_ERR;       // unrecognized command
+    }
+}
+
+static void make_response(const Response &resp, std::vector<uint8_t> &out) {
+    uint32_t resp_len = 4 + (uint32_t)resp.data.size();
+    buf_append(out, (const uint8_t *)&resp_len, 4);
+    buf_append(out, (const uint8_t *)&resp.status, 4);
+    buf_append(out, resp.data.data(), resp.data.size());
+}
+
+static int32_t parse_req(uint8_t *data,size_t size,std::vector<std::string> &out){
+    const uint8_t* end = data + size;
+    uint32_t nstr = 0;
+    if(!read_32(data,end,nstr)){
+        return -1;
+    }
+    if(nstr > k_max_msg){
+        return -1;
+    }
+    while(out.size() < nstr){
+        uint32_t len = 0;
+        if(!read_32(data,end,len)){
+            return -1;
+        }
+        out.push_back(std::string());
+        if(!read_str(data,end,len,out.back())){
+            return -1;
+        }
+    }
+    if(data!=end){
+        return -1;
+    }
+    return 0;
+
+}
+
+static bool try_one_req(Conn* conn){
+
+    if (conn->incoming.size() < 4) {
+        return false;   // want read
     }
     uint32_t len = 0;
-    memcpy(&len,conn->incoming.data(),4);
-
-    if(len>k_max_msg){
-        return false;
+    memcpy(&len, conn->incoming.data(), 4);
+    if (len > k_max_msg) {
+        conn->want_close = true;
+        return false;   // want close
     }
-
-    if(4+len > conn->incoming.size()){
-        return false;
+    // message body
+    if (4 + len > conn->incoming.size()) {
+        return false;   // want read
     }
+    uint8_t *request = &conn->incoming[4];
+    
 
-    const uint8_t *request = &conn->incoming[4];
     // 4. Process the parsed message.
     // ...
     // generate the response (echo)
+    std::vector<std::string> cmd;
+    if(parse_req(request,len,cmd) < 0){
+        conn->want_close = true;
+        return false;
+    }
+    Response resp;
+    do_request(cmd,resp);
+    make_response(resp,conn->outgoing);
+
+
+
     std::cout << "Client says: "
           << std::string((const char*)request, len)
           << "\n";
